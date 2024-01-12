c4c0a0259ce057794c9c8d357cad7b1b4758e170
alamin.147 <alamin.14780@gmail.com>
2024-01-12 22:22:13 +0600

    update code

---
+++ 1907A-Rook.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        //-'0'
+        string a;
+        cin >> a;
+        char b[] = "abcdefgh";
+        for (int i = 1; i <= 8; i++)
+        {
+            if ((a[1] - '0') == i)
+                continue;
+            else
+                cout << a[0] << i << endl;
+        }
+        for (int i = 0; i < 8; i++)
+        {
+            if (a[0] == b[i])
+                continue;
+            else
+                cout << (b[i]) << a[1] << endl;
+        }
+    }
+}
