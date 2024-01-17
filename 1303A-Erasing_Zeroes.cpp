6af66122ba4045031d9f27cadd752d3c30924aeb
alamin.147 <alamin.14780@gmail.com>
2024-01-18 01:34:23 +0600

    update code

---
+++ 1303A-Erasing_Zeroes.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string a;
+        cin >> a;
+        int c = 0;
+
+        for (int i = a.find('1'); i < a.find_last_of('1'); i++)
+        {
+            if (a[i] == '0')
+                c++;
+        }
+        cout << c << endl;
+    }
+}
