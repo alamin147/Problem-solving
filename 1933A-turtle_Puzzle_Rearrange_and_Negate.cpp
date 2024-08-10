c397414ba151028d88105abe64a023a111339b3e
alamin.147 <alamin.14780@gmail.com>
2024-08-10 11:07:28 +0600

    update

---
+++ 1933A-turtle_Puzzle_Rearrange_and_Negate.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define nl '\n'
+#define forn for (int i = 0; i < n; i++)
+#define fornj for (int j = 0; j < n; j++)
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int S = 0, j;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> j;
+            S += abs(j);
+        }
+        cout << S << nl;
+    }
+}
