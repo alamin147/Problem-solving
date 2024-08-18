f34c67689293de40f00f99e9c72bfca3988c2034
alamin.147 <alamin.14780@gmail.com>
2024-08-18 20:57:15 +0600

    update

---
+++ 1996A-legs.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define nl '\n'
+#define forn for (int i = 0; i < n; i++)
+#define fornj for (int j = 0; j < n; j++)
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        if (n <= 4)
+            cout << 1 << nl;
+        else
+        {
+            // 4 2
+            int s = 0;
+            s += n / 4;
+
+            n = n % 4;
+            s += n / 2;
+            cout << s << nl;
+        }
+    }
+}
